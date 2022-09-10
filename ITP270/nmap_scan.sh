b830e4c628d827e1c721420617c0eae4ea2dda08
diegokendall <dak2017@email.vccs.edu>
2022-09-10 12:43:45 -0400

    Tidying up on new logon.

--- ITP270/nmap_scan.sh
+++ ITP270/nmap_scan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Please enter an IP address: "
+echo "Enter an IP address: "
 
 read ip_address
 
-nmap -A -sV -vv $ip_address -oN nmap_output.txt
+nmap -Pn -sV -vv $ip_address -oN nmap_output.txt
