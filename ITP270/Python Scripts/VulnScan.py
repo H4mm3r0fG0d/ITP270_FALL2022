dc518ff6e6d1ebb9bc651b635ba1c47f60a034a9
diegokendall <dak2017@email.vccs.edu>
2022-10-29 13:56:38 -0400

    Updated Vulnerability Scanner to be more user-friendly.

--- ITP270/Python Scripts/VulnScan.py
+++ ITP270/Python Scripts/VulnScan.py
@@ -5,6 +5,7 @@ from IPy import IP
 import pyfiglet
 import subprocess
 import time
+from datetime import datetime
 
 #Clearing the terminal window
 subprocess.call('clear', shell=True)
@@ -26,7 +27,7 @@ def scan(target):
     #Defining the scan target function
     converted_ip = convert_ip(target)
     print('\n' + 'Scanning Target : ' + str(target))
-    for port in range(21,80):
+    for port in range(int(start_port),int(end_port)):
         scan_port(converted_ip, port)
 
 def convert_ip(ip):
@@ -48,15 +49,14 @@ def scan_port(ipaddress, port):
 #print open port >> If port is closed, pass (print nothing to terminal)
     try:
         sock = socket.socket()
-        sock.settimeout(.01)
+        sock.settimeout(0.1)
         sock.connect((ipaddress, port))
 
         try:
-            ports.append(port)
-            banner = sock.recv(1024).decode().strip("\n").strip("\r")
-            banners.append(banner)
+            banner = get_banner(sock)
+            print('[+] Port ' + str(port) + ' is Open ' + ' : ' + str(banner.decode().strip('\n')))
         except:
-            banners.append(' ')
+            print('[+] Port ' + str(port) + ' is Open ')
         sock.close()
     except:
         pass
@@ -65,6 +65,12 @@ def scan_port(ipaddress, port):
 #Line 58 prevents scan function from being called twice if script is imported into another module
 if __name__ == "__main__":
     targets = input('[+] Enter target(s) to scan (For multiple targets use a comma): ')
+    start_port = input('Enter the port to start the scan: ')
+    end_port = input('Enter the port to start the scan: ')
+    #check start time
+    time_start = datetime.now().replace(microsecond=0)
+    print("Scanning started at:" + str(time_start))
+
     if ',' in targets:
         for ip_address in targets.split(','):
             scan(ip_address.strip(' '))
@@ -79,3 +85,12 @@ with open('vulnerable_banners.txt' , 'r') as file:
             if line.strip() in banner:
                 print('[!!] VULNERABLE BANNER: "' + banner + '" ON PORT: ' + str(ports[count]))
         count += 1
+
+#check completed time
+time_completed = datetime.now().replace(microsecond=0)
+
+#Total time to scan
+time_total = time_completed - time_start
+
+print("Scanning ended at:" + str(time_completed))
+print("Scanning Completed in " + str(time_total))
