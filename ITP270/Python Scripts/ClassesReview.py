69faeea48c781001e05f90eb0d9b7f9fb0c5ca68
diegokendall <dak2017@email.vccs.edu>
2022-11-20 14:17:16 -0500

    "Finished Class Review Assignment."

--- ITP270/Python Scripts/ClassesReview.py
+++ ITP270/Python Scripts/ClassesReview.py
@@ -4,19 +4,30 @@ class Student:
         self.name = name
         self.year = year
         self.grades = []
-
+# Defining the add grade function
     def add_grade(self, grade):
-        self.grades = grade
+        if type(grade) == Grade:
+            self.grades.append(grade.score)
+
+# Creating and defining the Grade class
+class Grade:
+    minimum_passing = 65
+
+    def __init__(self, score):
+        self.score = score
 
 # Saving Student instances to variables
 roger = Student('Roger van der Weyden', 'year 10')
 sandro = Student('Sandro Botticelli', 'year 12')
 pieter = Student('Pieter Breugel the Elder', 'year 8')
 
+# Adding a new grade with score of 100 to Pieter's grade attribute
+pieter.add_grade(Grade(100))
 
 # Testing the instances
 #print(pieter.name)
 #print(pieter.year)
+#print(pieter.grades)
 
 #print(sandro.name)
 #print(sandro.year)
